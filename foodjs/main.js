f7a7f1fd78525dc3ab70dc21d892945430a0e46e
sudoaptrana <aryan.rana2021@vitbhopal.ac.in>
2022-08-02 00:58:58 +0530

    Strting 1-Aug

---
+++ foodjs/main.js
@@ -0,0 +1,24 @@
+console.log('hello world')
+console.log('sudoaptrana')
+
+name = 'sudoapt'
+console.log(name)
+
+sentence = 'how are you doing today, nice to see you, hope you have a great day!'
+console.log(sentence)
+
+//operators
+//fruit = prompt('What is your favourite fruit?')
+
+//console.log(fruit)
+
+// * / ** + -
+
+food = Number(prompt('how much was the food?'))
+tipPercentage = Number(prompt('tip%?')/100)
+tipAmount = food * tipPercentage
+total = food + tipAmount
+
+console.log('amount of tip', tipAmount)
+console.log('total amount' , total)
+alert(tipAmount)
