0333fac319575c1f860ad29fac7cb079d7bcb0aa
sudoaptrana <aryan.rana2021@vitbhopal.ac.in>
2022-08-22 23:51:58 +0530

    array

---
+++ Array/main.js
@@ -0,0 +1,29 @@
+const groceries = ['banana', 'apple', 'guava', 'papaya']
+
+console.log(groceries)
+console.log(groceries[0])
+
+//grab the 1st index
+console.log(groceries[1])
+
+//grab the 2nd index
+console.log(groceries[2])
+
+//Push() method
+groceries.push('grapes')
+console.log(groceries)
+
+groceries.push('avocardo')
+console.log(groceries)
+
+//Array Slice
+
+console.log(groceries.slice(0, 2))
+
+console.log(groceries.slice(2, 4))
+
+//indexOf method
+console.log(groceries.indexOf('apple')
+
+//length method
+console.log(groceries.length)
